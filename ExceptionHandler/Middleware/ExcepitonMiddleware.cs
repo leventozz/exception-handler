4ad1c0e45c54393f61725d351683656c4754a16f
leventozz <leventufukozturk@gmail.com>
2024-09-18 20:48:44 +0300

    Add project files.

---
+++ ExceptionHandler/Middleware/ExcepitonMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExceptionHandler.Middleware
+{
+    public class ExcepitonMiddleware : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            //Logging and other bussiness
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Something went wrong...",
+                Detail = exception.Message
+            };
+
+            httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+            await httpContext.Response
+                .WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+        }
+    }
+}
---
+++ ExceptionHandler/Program.cs
@@ -0,0 +1,59 @@
+﻿using ExceptionHandler.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<ExcepitonMiddleware>();
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //app.UseDeveloperExceptionPage();
+}
+else
+{
+    //open this comment block for using UseExceptionHandler without IExceptionHandler
+
+    //app.UseExceptionHandler(errorApp =>
+    //{
+    //    errorApp.Run(async context =>
+    //    {
+    //        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+    //        context.Response.ContentType = "text/html";
+
+    //        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+    //        if (exceptionHandlerPathFeature?.Error != null)
+    //        {
+    //            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    //            logger.LogError($"Something went wrong: {exceptionHandlerPathFeature.Error.Message}");
+    //        }
+
+    //        await context.Response.WriteAsync(exceptionHandlerPathFeature.Error.Message).ConfigureAwait(false);
+    //    });
+    //});
+
+    
+
+}
+
+app.UseExceptionHandler();
+app.UseHsts();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
